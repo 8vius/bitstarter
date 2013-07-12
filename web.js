4920b1ce63e3d392377e075ac7ee9ff419ca77fb
Luis Miguel Delgado <lmd411@gmail.com>
2013-07-11 23:22:45 -0430

    Updated web.js for index

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
-
+var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var content = fs.readFileSync('index.html', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+    });
+
+    var buf = new Buffer(content, "utf-8")
+
+    response.send(buf.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
